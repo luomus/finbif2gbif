ee3f3e3f056ada1bec92370bedf1f34e3655a464
Will <willi@mmorris.email>
2021-07-28 09:34:56 +0300

    Stop logging health check errors

--- init.R
+++ init.R
@@ -29,9 +29,9 @@ p$registerHooks(
 
       log_fn <- logger::log_info
 
-      if (identical(req$PATH_INFO, "/status")) log_fn <- function(.) {}
-
       if (res$status >= 400L) log_fn <- logger::log_error
+
+      if (identical(req$PATH_INFO, "/healthz")) log_fn <- function(.) {}
 
       log_fn(
         paste0(
