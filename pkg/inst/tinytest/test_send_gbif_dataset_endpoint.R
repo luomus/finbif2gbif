2302f708856f2d400eaa7b5e7dd85a244ffd3c6c
Will <willi@mmorris.email>
2021-08-03 11:13:54 +0300

    Add function to send dataset endpoint to GBIF

---
+++ pkg/inst/tinytest/test_send_gbif_dataset_endpoint.R
@@ -0,0 +1,16 @@
+gbif <- webfakes::new_app()
+
+gbif$post(
+  "/v1/dataset/1234/endpoint",
+  function(req, res) {
+    res$send("")
+  }
+)
+
+api <- webfakes::local_app_process(gbif)
+
+expect_identical(
+  send_gbif_dataset_endpoint(list(), "1234", api$url()), NULL
+)
+
+api$stop()
