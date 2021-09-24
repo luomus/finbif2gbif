998753ee5eac977e3290f70b455130338b0fc815
Will <willi@mmorris.email>
2021-09-24 14:30:46 +0300

    Fix test

--- pkg/inst/tinytest/test_get_metadata.R
+++ pkg/inst/tinytest/test_get_metadata.R
@@ -2,4 +2,13 @@ Sys.setenv(FINBIF_ACCESS_TOKEN = "dummy")
 
 options(finbif_allow_query = FALSE, finbif_cache_path = getwd())
 
-expect_inherits(get_metadata("HR.3991"), "list")
+expect_inherits(
+  get_metadata(
+    "HR.3991",
+    list(
+      language = "language",
+      license = "intellectual_rights"
+    )
+  ),
+  "list"
+)
