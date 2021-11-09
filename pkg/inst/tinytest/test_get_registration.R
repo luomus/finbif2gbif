cf5fc4af3aded10ebed298b5f2279ffee2f0f73f
wkmor1 <willi@mmorris.email>
2021-11-09 16:26:14 +0200

    Update test

--- pkg/inst/tinytest/test_get_registration.R
+++ pkg/inst/tinytest/test_get_registration.R
@@ -3,12 +3,16 @@ attr(x, "class") <- "registration"
 attr(x, "last_mod") <- as.Date(integer(0L))
 
 expect_equal(
-  get_registration(list(list(identifiers = list(list(identifier = "x")))), "x"),
+  get_registration(
+    list(list(identifiers = list(list(identifier = "http://tun.fi/x")))), "x"
+  ),
   x
 )
 
 expect_null(
-  get_registration(list(list(identifiers = list(list(identifier = "x")))), "y"),
+  get_registration(
+    list(list(identifiers = list(list(identifier = "http://tun.fi/x")))), "y"
+  ),
 )
 
 expect_equal(last_mod(x), as.Date(integer(0L)))
