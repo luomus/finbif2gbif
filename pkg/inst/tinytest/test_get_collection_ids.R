8d32a80b3cdb2553f4c3f2e67cdf5ad43cdf721a
wkmor1 <willi@mmorris.email>
2021-12-08 10:40:22 +0200

    Merge changes from 'dev'

--- pkg/inst/tinytest/test_get_collection_ids.R
+++ pkg/inst/tinytest/test_get_collection_ids.R
@@ -1,6 +1,23 @@
 source("setup.R")
 
-collections <- get_collection_ids()
+gbif <- webfakes::new_app()
+
+gbif$get(
+  "/v1/installation/abcd/dataset",
+  function(req, res) {
+    file <- "gbif_datasets"
+    ans <- readBin(file, "raw", n = file.info(file)$size)
+    res$send(ans)
+  }
+)
+
+api <- webfakes::local_app_process(gbif)
+
+datasets <- get_gbif_datasets(api$url(), "abcd")
+
+api$stop()
+
+collections <- get_collection_ids(datasets)
 
 expect_inherits(collections, "list")
 
