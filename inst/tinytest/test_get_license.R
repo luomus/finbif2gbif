2781ff7d1d9faca0f8482a2901918a31c26a9248
wkmor1 <willi@mmorris.email>
2026-07-18 10:42:36 +0300

    Update tests

--- inst/tinytest/test_get_license.R
+++ inst/tinytest/test_get_license.R
@@ -41,3 +41,45 @@ expect_inherits(
   ),
   "list"
 )
+
+expect_inherits(
+  f2g:::get_license_id(
+    "https://creativecommons.org/publicdomain/zero/1.0/legalcode"
+  ),
+  "list"
+)
+
+expect_inherits(
+  f2g:::get_license_id(
+    "https://creativecommons.org/licenses/by-nc/4.0/legalcode"
+  ),
+  "list"
+)
+
+expect_inherits(
+  f2g:::get_license_id(
+    "https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode"
+  ),
+  "list"
+)
+
+expect_inherits(
+  f2g:::get_license_id(
+    "https://creativecommons.org/licenses/by-sa/4.0/legalcode"
+  ),
+  "list"
+)
+
+expect_inherits(
+  f2g:::get_license_id(
+    "https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode"
+  ),
+  "list"
+)
+
+expect_inherits(
+  f2g:::get_license_id(
+    "https://creativecommons.org/licenses/by-nd/4.0/legalcode"
+  ),
+  "list"
+)
